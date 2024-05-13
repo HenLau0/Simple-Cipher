a460ae2ef6c86e823cff32cd1b7377d378d32840
HenLau0 <162377378+HenLau0@users.noreply.github.com>
2024-05-13 21:24:05 +0000

    finguring out why it wont alert the finished finstr

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-var ed = document.getElementById('ed').value
-var normtext = document.getElementById('normtext').value
-if (ed == "encode" || ed == "Encode" || ed == "e" || ed == "E"){
-    var ci1 = 0
-    var ci2 = 0
-    const alstr = "abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz "
-    //first letters cipher encoding
-    for (let i = 0; i < 28; i++) {
-        ci1 = i+1
-    }
-    for (let i = 0; i < 28; i++) {
-        ci2 = i+1
-    }
-    alert(ci1)
-    alert(ci2)
-    var encode = -(ci1-ci2)
-    print(encode)
+function translate() {
+    var ed = document.getElementById('ed').value
+    var normtext = document.getElementById('normtext').value
+    const alstr = "abcdefghijklmnopqrstuvwxyz abcdefghijklmnopqrstuvwxyz ";
+    var ci1 = 0;
+    var ci2 = 0;
+    if (ed == "encode" || ed == "Encode" || ed == "e" || ed == "E"){
+        alert("working")
+        //first letters cipher encoding
+        for (let i = -1; i < 28; i++) {
+            if (normtext[0] == alstr[i]) {
+                ci1 = i+1;
+            }
+        }
+        for (let t = -1; t < 28; t++) {
+            if (normtext[1] == alstr[t]) {
+                ci2 = t+1;
+                }
+        }
+        if (normtext[0] == "a" || normtext[0] == "A"){
+            ci1 = 1
+        }
+        var encode = (ci2-ci1);
+        alert(encode)
+        //encoding string
+        var finstr = [];
+        finstr.push(normtext[0]);
+        finstr.push(normtext[1]);
+        alert(finstr)
+        for (let r = -1; r < (length(normtext)+1); r++) {
+            for (let f = -1; f < 27; f++) {
+                if (normtext[r] == alstr[f]) {
+                    finstr.push(alstr[(f+encode)]);
+                }
+            }
+        }
+        alert(finstr)
+        /*for r in range(len(normtext)):
+        for f in range(26):
+            if normtext[r] == alstr[f]:
+                finstr.append(alstr[f+encode])*/
 }
+}
+document.getElementById('cipher-details').addEventListener("submit", function(e) {
+    // This prevents the default form submission function from happening (HTML default)
+    e.preventDefault();
+
+    // Run your function
+    translate();
+  }
+);
